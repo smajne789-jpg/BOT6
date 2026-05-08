5f2df6b294de5479fd38ff87fbfd39d416ae3acd
smajne789-jpg <smajne789@gmail.com>
2026-05-09 03:45:13 +0400

    Update telegram_finance_bot_single_file_aiogram (5).py

--- telegram_finance_bot_single_file_aiogram (5).py
+++ telegram_finance_bot_single_file_aiogram (5).py
@@ -30,8 +30,8 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 BOT_USERNAME = os.getenv("BOT_USERNAME")
-MIN_WITHDRAW = 100
-MAX_WITHDRAW = 50000
+MIN_WITHDRAW = 1
+MAX_WITHDRAW = 50
 
 bot = Bot(
     token=TOKEN,
